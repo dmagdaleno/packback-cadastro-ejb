9e7a9c82b718d99ff9c746d10a05119e992a87b4
dmagdaleno <diegommagdaleno@gmail.com>
2019-04-15 11:20:37 -0300

    implementa o método post na servlet para cadastro e edição de usuários

--- src/main/java/br/com/boomerang/packback/cadastro/controller/UsuarioController.java
+++ src/main/java/br/com/boomerang/packback/cadastro/controller/UsuarioController.java
@@ -2,18 +2,15 @@ package br.com.boomerang.packback.cadastro.controller;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
-import java.util.Set;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.boomerang.packback.cadastro.HistoricoPesquisasService;
 import br.com.boomerang.packback.cadastro.domain.Usuario;
+import br.com.boomerang.packback.cadastro.domain.builder.UsuarioBuilder;
 import br.com.boomerang.packback.cadastro.service.UsuarioService;
 
 @WebServlet(value = "/usuarios")
@@ -21,12 +18,12 @@ public class UsuarioController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	private UsuarioService usuarioService;
+	private UsuarioService service;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		
-		usuarioService = (UsuarioService) req.getSession().getAttribute("usuarioService");
+		service = (UsuarioService) req.getSession().getAttribute("service");
 
 		try {
 			String acao = req.getParameter("acao");
@@ -57,7 +54,7 @@ public class UsuarioController extends HttpServlet {
 			throws ServletException, IOException {
 		Long id = Long.parseLong(req.getParameter("id"));
 		try {
-			Usuario usuario = usuarioService.buscaPor(id);
+			Usuario usuario = service.buscaPor(id);
 			req.setAttribute("usuario", usuario);
 		} catch (Exception e) {
 			req.setAttribute("erro", "Não foi possível carregar dados para edição.");
@@ -68,11 +65,107 @@ public class UsuarioController extends HttpServlet {
 	private void listar(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		try {
-			Collection<Usuario> usuarios = usuarioService.listar();
+			Collection<Usuario> usuarios = service.listar();
 			req.setAttribute("usuarios", usuarios);
 		} catch (Exception e) {
 			req.setAttribute("erro", "Não foi possível carregar os registros.");
 		}
 		req.getRequestDispatcher("templates/lista/peso.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		service = (UsuarioService) req.getSession().getAttribute("service");
+
+		try {
+			String acao = req.getParameter("acao");
+
+			if(acao == null)
+				throw new IllegalArgumentException("Ação não pode ser nula");
+
+			switch (acao) {
+				case "cadastrar":
+					cadastrar(req, resp);
+					break;
+
+				case "editar":
+					editar(req, resp);
+					break;
+
+				case "excluir":
+					excluir(req, resp);
+					break;
+
+				default:
+					break;
+			}
+		}
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			req.setAttribute("erro", e.getMessage());
+		}
+
+	}
+
+	private void cadastrar(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+
+			Usuario usuario = constroiUsuario(req);
+
+			service.cadastrar(usuario);
+
+			req.setAttribute("sucesso", "Cadastro realizado com sucesso!");
+			listar(req, resp);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			req.setAttribute("erro", "Não foi possível realizar o cadastro.");
+			req.getRequestDispatcher("templates/cadastro/usuario.jsp").forward(req, resp);
+		}
+
+	}
+
+	private Usuario constroiUsuario(HttpServletRequest req) {
+		Long id = Long.parseLong(req.getParameter("id"));
+		String nome = req.getParameter("nome");
+		String email = req.getParameter("email");
+		String cpf = req.getParameter("cpf");
+		String cnpj = req.getParameter("cnpj");
+		String razaoSocial = req.getParameter("razaoSocial");
+
+		return new UsuarioBuilder().comId(id).comNome(nome).comCpf(cpf).comCnpj(cnpj).comEmail(email).comRazaoSocial(razaoSocial).constroi();
+	}
+
+	private void editar(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			Usuario usuario = constroiUsuario(req);
+
+			service.atualizar(usuario);
+
+			req.setAttribute("sucesso", "Registro editado com sucesso!");
+			listar(req, resp);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			req.setAttribute("erro", "Não foi possível editar o registro.");
+			req.getRequestDispatcher("templates/edicao/peso.jsp").forward(req, resp);
+		}
+	}
+
+	private void excluir(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			Long id = Long.parseLong(req.getParameter("id"));
+
+			service.excluir(id);
+
+			req.setAttribute("sucesso", "Registro excluído com sucesso!");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			req.setAttribute("erro", "Não foi possível excluir o registro.");
+		}
+
+		listar(req, resp);
 	}
 }
