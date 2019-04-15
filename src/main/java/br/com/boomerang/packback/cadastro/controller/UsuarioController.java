09af3021f0473b884989415b5f53b181d0853a6a
dmagdaleno <diegommagdaleno@gmail.com>
2019-04-15 10:25:04 -0300

    adiciona métodos de busca para o controller

--- src/main/java/br/com/boomerang/packback/cadastro/controller/UsuarioController.java
+++ src/main/java/br/com/boomerang/packback/cadastro/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.boomerang.packback.cadastro.controller;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -12,52 +13,66 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.boomerang.packback.cadastro.HistoricoPesquisasService;
-import br.com.boomerang.packback.cadastro.UsuarioService;
+import br.com.boomerang.packback.cadastro.domain.Usuario;
+import br.com.boomerang.packback.cadastro.service.UsuarioService;
 
 @WebServlet(value = "/usuarios")
 public class UsuarioController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
+	private UsuarioService usuarioService;
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		
-		UsuarioService usuarioService = (UsuarioService) req.getSession().getAttribute("usuarioService");
-		
-		HistoricoPesquisasService historicoService = (HistoricoPesquisasService) req.getSession().getAttribute("historicoService");
-		
-		String termo = req.getParameter("termo");
-		
-		this.pesquisar(termo, req, usuarioService);
-		this.registrarHistorico(termo, req, historicoService);
-		
-		RequestDispatcher dispacher = req.getRequestDispatcher("usuarios.jsp");
-		dispacher.forward(req, resp);
+		usuarioService = (UsuarioService) req.getSession().getAttribute("usuarioService");
+
+		try {
+			String acao = req.getParameter("acao");
+
+			if(acao == null)
+				throw new IllegalArgumentException("Ação não pode ser nula");
+
+			switch (acao) {
+				case "listar":
+					listar(req, resp);
+					break;
+
+				case "editar":
+					abrirFormularioEdicao(req, resp);
+					break;
+
+				default:
+					break;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			req.setAttribute("erro", e.getMessage());
+		}
 	}
-	
-	private void pesquisar(String termo, HttpServletRequest req, UsuarioService usuarioService) {
-		List<String> resultadoPesquisa = usuarioService.pesquisar(termo);
-		
-		StringBuilder sbResultado = new StringBuilder();
-		
-		for (String resultado : resultadoPesquisa) 
-			sbResultado.append("<li>" + resultado + "</li>");
-		
-		req.setAttribute("termo", termo);
-		req.setAttribute("encontrados", resultadoPesquisa.size());
-		req.setAttribute("resultado", sbResultado.toString());
+
+	private void abrirFormularioEdicao(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		Long id = Long.parseLong(req.getParameter("id"));
+		try {
+			Usuario usuario = usuarioService.buscaPor(id);
+			req.setAttribute("usuario", usuario);
+		} catch (Exception e) {
+			req.setAttribute("erro", "Não foi possível carregar dados para edição.");
+		}
+		req.getRequestDispatcher("templates/edicao/peso.jsp").forward(req, resp);
 	}
-	
-	private void registrarHistorico(String termo, HttpServletRequest req, HistoricoPesquisasService historicoService) {
-		historicoService.registrarPesquisa(termo);
-		
-		Set<String> pesquisas = historicoService.getHistorico();
-		
-		StringBuilder sbHistorico = new StringBuilder();
-		
-		for (String pesquisa : pesquisas) 
-			sbHistorico.append("<li><i>" + pesquisa + "</i></li>");
-		
-		req.setAttribute("historico", sbHistorico);
+
+	private void listar(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			Collection<Usuario> usuarios = usuarioService.listar();
+			req.setAttribute("usuarios", usuarios);
+		} catch (Exception e) {
+			req.setAttribute("erro", "Não foi possível carregar os registros.");
+		}
+		req.getRequestDispatcher("templates/lista/peso.jsp").forward(req, resp);
 	}
 }
